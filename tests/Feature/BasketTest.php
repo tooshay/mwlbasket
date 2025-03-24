feb76340ae00d08ab4ed72cd3a6e3f6638a389ab
Roy Shay <roy.shay@gmail.com>
2025-03-24 11:56:19 +0000

    add test

--- tests/Feature/BasketTest.php
+++ tests/Feature/BasketTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Product;
+use Illuminate\Testing\Fluent\AssertableJson;
 it('returns a successful response', function () {
     $response = $this->get('/');
 
@@ -11,5 +13,27 @@ it('returns return items for an existing basket', function () {
 
     $response = $this->json('GET', route('basket.get'));
 
-    $this->assertEquals(200, $response->status());
+    $response->assertStatus(200);
+});
+
+it('adds an item to the authenticated user basket', function () {
+    createAuthedUserWithFullBasket();
+    $product = Product::first();
+
+    // Act
+    $response = postJson('/items', [
+        'product_id' => $product->id,
+    ]);
+
+    // Assert
+    $response->assertCreated();
+
+    $response->assertJson(fn (AssertableJson $json) =>
+    $json->where('product_id', $product->id)
+        ->where('status', 'ADDED')
+        ->hasAll(['id', 'basket_id', 'product_id', 'status', 'created_at', 'updated_at'])
+    );
+
+    expect(Item::count())->toBe(1);
+    expect(Item::first())->product_id->toBe($product->id);
 });
